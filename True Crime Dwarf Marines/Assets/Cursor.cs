59033fdb91347599b016a57ea860d95d8dec7afb
Keanan Pucci <kcp320@nyu.edu>
2017-04-28 11:59:57 -0400

    Point and Click Prototype

---
+++ True Crime Dwarf Marines/Assets/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	//public GameObject Cursor;
+	public Vector3 mousePos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.3f));
+		this.transform.position = mousePos;
+	}
+}
---
+++ True Crime Dwarf Marines/Assets/Objecto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objecto : MonoBehaviour {
+
+	public bool hover = false;
+	public bool click = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		click=Input.GetMouseButtonDown(0);
+		if (hover==true&&click==true){
+			Debug.Log("clicked");
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		hover=true;
+	}
+	void OnTriggerExit2D(Collider2D other){
+		hover=false;
+	}
+}
